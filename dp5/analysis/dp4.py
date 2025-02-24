78739bc3b19f78b802a1177509817c96c1d27f4d
Benji Rowlands <jbr46@cam.ac.uk>
2025-02-24 14:56:52 +0000

    Get rid of debug prints in dp4.py

--- dp5/analysis/dp4.py
+++ dp5/analysis/dp4.py
@@ -111,9 +111,6 @@ class DP4:
             mol_dict.update(H_dict)
             H_data.append(H_dp4)
 
-            print(f'C SHIFTS: {mol.C_shifts}')
-            print(f'C EXPERIMENTAL: {mol.C_exp}')
-            print(f'C LABELS: {mol.C_labels}')
             *C_metadata, C_dp4 = self.dp4_carbon(mol.C_shifts, mol.C_exp, mol.C_labels)
             C_dict = {k: v for k, v in zip(C_keys, C_metadata)}
             mol_dict.update(C_dict)
