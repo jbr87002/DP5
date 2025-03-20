74dbd677c7368a1ce9fcc4f6691a1180898698c7
Benji Rowlands <jbr46@cam.ac.uk>
2025-03-20 16:24:53 +0000

    Update options

--- examples/run_script.py
+++ examples/run_script.py
@@ -11,12 +11,15 @@ from dp5.load_config import run_workflow
 from rdkit import Chem
 
 nmr_file = "/scratch/jbr46/np_atlas_nmr/NMR_example"
-precalculated_sdf = "/scratch/jbr46/np_atlas_nmr/nmr_shifts_sgnn.sdf"
+precalculated_sdf = "/scratch/jbr46/np_atlas_nmr/nmr_shifts.sdf"
+output_path = "/scratch/jbr46/np_atlas_nmr/dp5_precalc_testing"
 data = run_workflow(
-    structure_files=['/scratch/jbr46/np_atlas_nmr/np_atlas_1000.smi'],
+    structure_files=['/scratch/jbr46/np_atlas_nmr/np_atlas_100.smi'],
     nmr_files=[nmr_file],
+    output_path=output_path,
     input_type='smiles',
-    workflow='s',  # DP4 analysis
+    workflow='w',  # DP4 analysis,
+    remove_previous=True,
     precalculated_sdf=precalculated_sdf,
-    model='sgnn'
+    model='cascade'
 )
